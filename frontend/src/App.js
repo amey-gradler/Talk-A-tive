5606986f99bc52483812d9985d4942c3e8c0a22a
ameyA <ameyaganore@gmail.com>
2022-02-11 15:19:26 +0530

    authentication frontend done7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { Button, ButtonGroup } from '@chakra-ui/react';
 import { Route } from 'react-router-dom';
-
+import './App.css';
 import HomePage from './Pages/HomePage';
 import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Route path='/' component={HomePage} exact />
       <Route path='/chats' component={ChatPage} exact />
     </div>
